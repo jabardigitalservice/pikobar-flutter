e83a076fa04d32573591c1178e443ac7ac72a6f8
aldi <aldirohman@live.com>
2020-03-17 08:54:33 +0700

    comment local notification

--- lib/utilities/NotificationHelper.dart
+++ lib/utilities/NotificationHelper.dart
@@ -1,35 +1,35 @@
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:meta/meta.dart';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:meta/meta.dart';
 
-typedef SelectNotificationCallback = Future<dynamic> Function(String payload);
+// typedef SelectNotificationCallback = Future<dynamic> Function(String payload);
 
-class NotificationHelper {
+// class NotificationHelper {
 
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+//   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
-  showNotification(String title, String body, {String payload, @required SelectNotificationCallback onSelectNotification}) async {
+//   showNotification(String title, String body, {String payload, @required SelectNotificationCallback onSelectNotification}) async {
 
-    var initializationSettingsAndroid =
-    AndroidInitializationSettings('@mipmap/ic_launcher');
-    var initializationSettingsIOS = IOSInitializationSettings();
-    var initializationSettings = InitializationSettings(
-        initializationSettingsAndroid, initializationSettingsIOS);
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onSelectNotification: onSelectNotification);
+//     var initializationSettingsAndroid =
+//     AndroidInitializationSettings('@mipmap/ic_launcher');
+//     var initializationSettingsIOS = IOSInitializationSettings();
+//     var initializationSettings = InitializationSettings(
+//         initializationSettingsAndroid, initializationSettingsIOS);
+//     await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+//         onSelectNotification: onSelectNotification);
 
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        'sapawarga_channel_id', 'sapawarga_channel', 'sapawarga_channel_description',
-        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
-    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-    var platformChannelSpecifics = NotificationDetails(
-        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-        0, title, body, platformChannelSpecifics,
-        payload: payload);
-  }
+//     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+//         'sapawarga_channel_id', 'sapawarga_channel', 'sapawarga_channel_description',
+//         importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+//     var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+//     var platformChannelSpecifics = NotificationDetails(
+//         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+//     await flutterLocalNotificationsPlugin.show(
+//         0, title, body, platformChannelSpecifics,
+//         payload: payload);
+//   }
 
-  NotificationHelper() {
-    this.flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  }
+//   NotificationHelper() {
+//     this.flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+//   }
 
-}
+// }
