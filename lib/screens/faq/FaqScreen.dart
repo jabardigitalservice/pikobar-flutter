7ddfdb2005fc9c1ecd46b5e8d5813b5622fa3cc1
aldi <aldirohman@live.com>
2020-03-18 13:41:56 +0700

    add ui search

--- lib/screens/faq/FaqScreen.dart
+++ lib/screens/faq/FaqScreen.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:html/dom.dart' as dom;
+import 'package:pikobar_flutter/components/CustomAppBar.dart';
 import 'package:pikobar_flutter/components/Expandable.dart';
 import 'package:pikobar_flutter/components/Skeleton.dart';
 import 'package:pikobar_flutter/constants/Dictionary.dart';
@@ -13,17 +16,37 @@ class FaqScreen extends StatefulWidget {
 }
 
 class _FaqScreenState extends State<FaqScreen> {
+  AnimationController _animationController;
+  final _searchController = TextEditingController();
+  Timer _debounce;
+
+  bool _isSearch = false;
+  final containerWidth = 40.0;
+
+  final _nodeOne = FocusNode();
+
   @override
   void initState() {
+    _searchController.addListener((() {
+      _onSearchChanged();
+    }));
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(Dictionary.faq),
-      ),
+      appBar: AppBar(title: Text(Dictionary.faq), actions: [
+        IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('masuk');
+              // Navigator.of(context).push(PageTransition(
+              //     child: ImportantInfoSearchScreen(),
+              //     type: PageTransitionType.fade));
+            }),
+      ]),
       body: StreamBuilder<QuerySnapshot>(
         stream: Firestore.instance.collection(Collections.faq).snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -42,6 +65,79 @@ class _FaqScreenState extends State<FaqScreen> {
         },
       ),
     );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(title: _titleBar(), actions: <Widget>[
+      RotationTransition(
+        turns: Tween(begin: 0.0, end: 1.0).animate(_animationController),
+        child: IconButton(
+          icon: _isSearch ? Icon(Icons.close) : Icon(Icons.search),
+          onPressed: () {
+            // _searchPressed();
+          },
+        ),
+      ),
+      // action button
+    ]);
+  }
+
+  Widget _titleBar() {
+    return Stack(
+      children: <Widget>[
+        Align(
+          alignment: Alignment.centerRight,
+          child: AnimatedOpacity(
+            opacity: _isSearch ? 1.0 : 0.0,
+            duration: Duration(milliseconds: 500),
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              width: containerWidth,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: TextField(
+                controller: _searchController,
+                autofocus: false,
+                focusNode: _nodeOne,
+                textInputAction: TextInputAction.go,
+                style: TextStyle(color: Colors.black, fontSize: 16.0),
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0)),
+              ),
+            ),
+          ),
+        ),
+        Align(
+            alignment: Alignment.centerLeft,
+            child:
+                Visibility(visible: !_isSearch, child: Text(Dictionary.faq))),
+      ],
+    );
+  }
+
+  void _onSearchChanged() {
+    if (_debounce?.isActive ?? false) _debounce.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      if (_searchController.text.trim().isNotEmpty) {
+        print(_searchController.text);
+        // _resultList.clear();
+        // _page = 1;
+        // _importantInfoSearchBloc
+        //     .add(ImportantInfoSearch(_searchController.text, _page));
+
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 }
 
