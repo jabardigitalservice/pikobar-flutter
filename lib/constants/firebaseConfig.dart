26f42be82f7d687787e6e60ccea3663438ab1bc2
Risa Herdianto <aputra.zha@gmail.com>
2020-03-19 18:41:59 +0700

    - Imrprove menus - Implement remote config on menus

--- lib/constants/firebaseConfig.dart
+++ lib/constants/firebaseConfig.dart
@@ -2,4 +2,34 @@ class FirebaseConfig {
   static String storeUrl = 'force_update_store_url';
   static String currentVersion = 'force_update_current_version';
   static String forceUpdateRequired = 'force_update_required';
+
+  /// Remote config parameter key
+  /// for menus
+  static String jshUrl = 'jabarsaberhoax_url';
+  static String jshCaption = 'jabarsaberhoax_caption';
+  static String pikobarCaption = 'pikobar_caption';
+  static String pikobarUrl = 'pikobar_url';
+  static String worldInfoCaption = 'world_info_caption';
+  static String worldInfoUrl = 'world_info_url';
+  static String nationalInfoCaption = 'national_info_caption';
+  static String nationalInfoUrl = 'national_info_url';
+  static String donationCaption = 'donasi_caption';
+  static String donationUrl = 'donasi_url';
+  static String logisticCaption = 'logistic_caption';
+  static String logisticUrl = 'logistic_url';
+  static String reportEnabled = 'lapor_enabled';
+  static String reportCaption = 'lapor_caption';
+  static String reportUrl = 'lapor_url';
+  static String qnaEnabled = 'tanyajawab_enabled';
+  static String qnaCaption = 'tanyajawab_caption';
+  static String qnaUrl = 'tanyajawab_url';
+  static String selfTracingCaption = 'selftracing_caption';
+  static String selfTracingUrl = 'selftracing_url';
+  static String selfTracingEnabled = 'selftracing_enabled';
+  static String volunteerEnabled = 'volunteer_enabled';
+  static String volunteerCaption = 'volunteer_caption';
+  static String volunteerUrl = 'volunteer_url';
+  static String selfDiagnoseEnabled = 'selfdiagnose_enabled';
+  static String selfDiagnoseUrl = 'selfdiagnose_url';
+  static String selfDiagnoseCaption = 'selfdiagnose_caption';
 }
