4f1d939fd158249b652e3d6883ff2591d6ffeca0
jaluowen <48124139+jaluowen@users.noreply.github.com>
2020-03-27 12:31:01 +0700

    add validation

--- lib/utilities/Validations.dart
+++ lib/utilities/Validations.dart
@@ -2,12 +2,17 @@ import 'package:pikobar_flutter/constants/Dictionary.dart';
 
 class Validations {
   static String phoneValidation(String val) {
-   
+    Pattern pattern = r"^(^62\s?|^8)(\d{5,13})$";
+
+    RegExp regex = RegExp(pattern);
+
     if (val.isEmpty) return Dictionary.errorEmptyPhone;
 
     if (val.length < 3) return Dictionary.errorMinimumPhone;
 
     if (val.length > 13) return Dictionary.errorMaximumPhone;
+    
+    if (!regex.hasMatch(val)) return Dictionary.errorInvalidPhone;
 
     
 
