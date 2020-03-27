a9e725bab83c3616cf1dcb87fc65c1712e5a4748
aldi <aldirohman@live.com>
2020-03-27 23:55:06 +0700

    add event analytics  share

--- lib/screens/infoGraphics/infoGraphicsServices.dart
+++ lib/screens/infoGraphics/infoGraphicsServices.dart
@@ -3,7 +3,9 @@ import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
 import 'package:esys_flutter_share/esys_flutter_share.dart';
+import 'package:pikobar_flutter/constants/Analytics.dart';
 import 'package:pikobar_flutter/constants/Dictionary.dart';
+import 'package:pikobar_flutter/utilities/AnalyticsHelper.dart';
 
 class InfoGraphicsServices {
   Future<void> shareInfoGraphics(title, image) async {
@@ -13,6 +15,9 @@ class InfoGraphicsServices {
       Uint8List bytes = await consolidateHttpClientResponseBytes(response);
       await Share.file(Dictionary.appName, '$title.jpg', bytes, 'image/jpg',
           text: title);
+
+      await AnalyticsHelper.setLogEvent(Analytics.tappedInfoGraphicsShare,
+          <String, dynamic>{'title': title, 'image': image});
     } catch (e) {
       print('error: $e');
     }
