51d77bbd025cb65f2549a54b59d3dcb766433ae2
Risa Herdianto <aputra.zha@gmail.com>
2020-03-30 13:21:37 +0700

    Added custom user agent

--- lib/components/BrowserScreen.dart
+++ lib/components/BrowserScreen.dart
@@ -50,6 +50,7 @@ import 'package:pikobar_flutter/constants/Dictionary.dart';
                    initialHeaders: {},
                    initialOptions: InAppWebViewWidgetOptions(
                        inAppWebViewOptions: InAppWebViewOptions(
+                         userAgent: 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19',
                          debuggingEnabled: true,
                        )
                    ),
