dc49a07d87cf30248ea22ad9d80099f90d1b2c18
Angga <gaganugraha33@gmail.com>
2020-04-12 22:26:56 +0700

    Update DocumentListScreen.dart

--- lib/screens/document/DocumentListScreen.dart
+++ lib/screens/document/DocumentListScreen.dart
@@ -64,113 +64,115 @@ class _DocumentListScreenState extends State<DocumentListScreen> {
   }
 
   Widget _buildContent(List<DocumentSnapshot> dataDocuments) {
-    return ListView(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-              color: Colors.grey[200],
-              border: Border.all(color: Colors.grey[200]),
-              borderRadius: BorderRadius.circular(4.0)),
-          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-          margin: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-          child: Row(
-            children: <Widget>[
-              SizedBox(width: 10),
-              Text(
-                Dictionary.date,
-                style: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w600),
-                textAlign: TextAlign.left,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              SizedBox(width: 35),
-              Text(
-                Dictionary.titleDocument,
-                style: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w600),
-                textAlign: TextAlign.left,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              Container()
-            ],
+    return SingleChildScrollView(
+      child: ListView(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.grey[200]),
+                borderRadius: BorderRadius.circular(4.0)),
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+            margin: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+            child: Row(
+              children: <Widget>[
+                SizedBox(width: 10),
+                Text(
+                  Dictionary.date,
+                  style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.left,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(width: 35),
+                Text(
+                  Dictionary.titleDocument,
+                  style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.left,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Container()
+              ],
+            ),
           ),
-        ),
-        ListView.builder(
-            padding: const EdgeInsets.only(
-                left: 16.0, right: 16.0, bottom: 16.0, top: 10.0),
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: dataDocuments.length,
-            itemBuilder: (context, index) {
-              final DocumentSnapshot document = dataDocuments[index];
+          ListView.builder(
+              padding: const EdgeInsets.only(
+                  left: 16.0, right: 16.0, bottom: 16.0, top: 10.0),
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: dataDocuments.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot document = dataDocuments[index];
 
-              return Container(
-                  child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(width: 10),
-                      Text(
-                        unixTimeStampToDateDocs(
-                            document['published_at'].seconds),
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.w600),
-                        textAlign: TextAlign.left,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      SizedBox(width: 30),
-                      Expanded(
-                        child: InkWell(
-                          onTap: () {
-                            _downloadAttachment(
-                                document['title'], document['document_url']);
-                          },
-                          child: Text(
-                            document['title'],
-                            style: TextStyle(
-                                color: Colors.blue,
-                                decoration: TextDecoration.underline,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.w600),
-                            textAlign: TextAlign.left,
+                return Container(
+                    child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(width: 10),
+                        Text(
+                          unixTimeStampToDateDocs(
+                              document['published_at'].seconds),
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.w600),
+                          textAlign: TextAlign.left,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(width: 30),
+                        Expanded(
+                          child: InkWell(
+                            onTap: () {
+                              _downloadAttachment(
+                                  document['title'], document['document_url']);
+                            },
+                            child: Text(
+                              document['title'],
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w600),
+                              textAlign: TextAlign.left,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        child: IconButton(
-                          icon: Icon(FontAwesomeIcons.solidShareSquare,
-                              size: 17, color: Color(0xFF27AE60)),
-                          onPressed: () {
-                            DocumentServices().shareDocument(
-                                document['title'], document['document_url']);
-                          },
-                        ),
-                      )
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10, bottom: 10),
-                    color: Colors.grey[200],
-                    width: MediaQuery.of(context).size.width,
-                    height: 1.5,
-                  )
-                ],
-              ));
-            })
-      ],
+                        Container(
+                          child: IconButton(
+                            icon: Icon(FontAwesomeIcons.solidShareSquare,
+                                size: 17, color: Color(0xFF27AE60)),
+                            onPressed: () {
+                              DocumentServices().shareDocument(
+                                  document['title'], document['document_url']);
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 10, bottom: 10),
+                      color: Colors.grey[200],
+                      width: MediaQuery.of(context).size.width,
+                      height: 1.5,
+                    )
+                  ],
+                ));
+              })
+        ],
+      ),
     );
   }
 
@@ -192,52 +194,51 @@ class _DocumentListScreenState extends State<DocumentListScreen> {
             itemBuilder: (context, index) {
               return Container(
                   child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Skeleton(
-                            height: 20.0,
-                            width: 40,
-                            padding: 10.0,
-                          ),
-                          Expanded(
-                            child: Container(
-                              padding: const EdgeInsets.only(left: 16.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Skeleton(
-                                    height: 20.0,
-                                    width:
-                                    MediaQuery.of(context).size.width / 1.6,
-                                    padding: 10.0,
-                                  ),
-                                ],
+                      Skeleton(
+                        height: 20.0,
+                        width: 40,
+                        padding: 10.0,
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(left: 16.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Skeleton(
+                                height: 20.0,
+                                width: MediaQuery.of(context).size.width / 1.6,
+                                padding: 10.0,
                               ),
-                            ),
+                            ],
                           ),
-                          Container(
-                            child: Skeleton(
-                              height: 30.0,
-                              width: 30.0,
-                              padding: 10.0,
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
                       Container(
-                        margin: EdgeInsets.only(top: 20, bottom: 20),
                         child: Skeleton(
-                          height: 1.5,
-                          width: MediaQuery.of(context).size.width,
+                          height: 30.0,
+                          width: 30.0,
                           padding: 10.0,
                         ),
-                      )
+                      ),
                     ],
-                  ));
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20, bottom: 20),
+                    child: Skeleton(
+                      height: 1.5,
+                      width: MediaQuery.of(context).size.width,
+                      padding: 10.0,
+                    ),
+                  )
+                ],
+              ));
             })
       ],
     );
@@ -271,8 +272,8 @@ class _DocumentListScreenState extends State<DocumentListScreen> {
         url: url,
         savedDir: dir,
         fileName: name,
-        showNotification:
-            true, // show download progress in status bar (for Android)
+        showNotification: true,
+        // show download progress in status bar (for Android)
         openFileFromNotification:
             true, // click on notification to open downloaded file (for Android)
       );
