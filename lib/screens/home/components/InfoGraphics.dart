1a873e48a211cc8391f16c689f4b0b644986b436
Angga <gaganugraha33@gmail.com>
2020-05-28 11:13:18 +0700

    redesign info prakiral

--- lib/screens/home/components/InfoGraphics.dart
+++ lib/screens/home/components/InfoGraphics.dart
@@ -8,6 +8,7 @@ import 'package:pikobar_flutter/blocs/infographics/Bloc.dart';
 import 'package:pikobar_flutter/components/HeroImagePreviewScreen.dart';
 import 'package:pikobar_flutter/components/Skeleton.dart';
 import 'package:pikobar_flutter/constants/Analytics.dart';
+import 'package:pikobar_flutter/constants/Colors.dart';
 import 'package:pikobar_flutter/constants/Dictionary.dart';
 import 'package:pikobar_flutter/constants/FontsFamily.dart';
 import 'package:pikobar_flutter/constants/Navigation.dart';
@@ -28,6 +29,7 @@ class _InfoGraphicsState extends State<InfoGraphics> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
+
         Padding(
           padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 10.0),
           child: Row(
@@ -46,7 +48,7 @@ class _InfoGraphicsState extends State<InfoGraphics> {
                 child: Text(
                   Dictionary.more,
                   style: TextStyle(
-                      color: Color(0xFF828282),
+                      color: ColorBase.green,
                       fontWeight: FontWeight.w600,
                       fontFamily: FontsFamily.productSans,
                       fontSize: 14.0),
@@ -61,9 +63,24 @@ class _InfoGraphicsState extends State<InfoGraphics> {
             ],
           ),
         ),
+
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+          child: Text(
+            Dictionary.descInfoGraphic,
+            style: TextStyle(
+                color: Colors.grey,
+                fontSize: 12.0,
+                fontWeight: FontWeight.w600),
+            textAlign: TextAlign.left,
+          ),
+        ),
+
         BlocBuilder<InfoGraphicsListBloc, InfoGraphicsListState>(
           builder: (context, state) {
-            return state is InfoGraphicsListLoaded ? _buildContent(state.infoGraphicsList) : _buildLoading();
+            return state is InfoGraphicsListLoaded
+                ? _buildContent(state.infoGraphicsList)
+                : _buildLoading();
           },
         )
       ],
@@ -130,60 +147,46 @@ class _InfoGraphicsState extends State<InfoGraphics> {
   }
 
   Widget _buildContent(List<DocumentSnapshot> listData) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        itemCount: listData.length,
-        itemBuilder: (context, index) {
-          final DocumentSnapshot document = listData[index];
-
-          return Container(
-            padding: const EdgeInsets.only(bottom: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                InkWell(
-                  child: Container(
-                    width: 70,
-                    height: 70,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: CachedNetworkImage(
-                        imageUrl: document['images'][0]??'',
-                        alignment: Alignment.topCenter,
-                        fit: BoxFit.cover,
-                        placeholder: (context, url) => Center(
-                            heightFactor: 4.2,
-                            child: CupertinoActivityIndicator()),
-                        errorWidget: (context, url, error) => Container(
-                          height: MediaQuery.of(context).size.height / 3.3,
-                          color: Colors.grey[200],
-                          child: Image.asset(
-                              '${Environment.iconAssets}pikobar.png',
-                              fit: BoxFit.fitWidth),
+    return Container(
+      height: 268,
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+          padding: const EdgeInsets.only(
+              left: 11.0, right: 16.0, top: 16.0, bottom: 16.0),
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+//          physics: NeverScrollableScrollPhysics(),
+          itemCount: listData.length,
+          itemBuilder: (context, index) {
+            final DocumentSnapshot document = listData[index];
+            return Container(
+              padding: EdgeInsets.only(left: 10),
+              width: 150,
+              child: Column(
+                children: <Widget>[
+                  InkWell(
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: CachedNetworkImage(
+                          imageUrl: document['images'][0] ?? '',
+                          alignment: Alignment.topCenter,
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) => Center(
+                              heightFactor: 4.2,
+                              child: CupertinoActivityIndicator()),
+                          errorWidget: (context, url, error) => Container(
+                            height: MediaQuery.of(context).size.height / 3.3,
+                            color: Colors.grey[200],
+                            child: Image.asset(
+                                '${Environment.iconAssets}pikobar.png',
+                                fit: BoxFit.fitWidth),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => HeroImagePreview(
-                            Dictionary.heroImageTag,
-                            galleryItems: document['images'],
-                          ),
-                        ));
-
-                    AnalyticsHelper.setLogEvent(
-                        Analytics.tappedInfoGraphicsDetail,
-                        <String, dynamic>{'title': document['title']});
-                  },
-                ),
-                Expanded(
-                  child: InkWell(
                     onTap: () {
                       Navigator.push(
                           context,
@@ -198,50 +201,77 @@ class _InfoGraphicsState extends State<InfoGraphics> {
                           Analytics.tappedInfoGraphicsDetail,
                           <String, dynamic>{'title': document['title']});
                     },
-                    child: Container(
-                      padding: const EdgeInsets.only(left: 16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            document['title'],
-                            style: TextStyle(
-                                fontSize: 14.0, fontWeight: FontWeight.w600),
-                            textAlign: TextAlign.left,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => HeroImagePreview(
+                                    Dictionary.heroImageTag,
+                                    galleryItems: document['images'],
+                                  ),
+                                ));
+
+                            AnalyticsHelper.setLogEvent(
+                                Analytics.tappedInfoGraphicsDetail,
+                                <String, dynamic>{'title': document['title']});
+                          },
+                          child: Container(
+                            child: Column(
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      unixTimeStampToDateWithoutDay(
+                                          document['published_date'].seconds),
+                                      style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 12.0,
+                                          fontWeight: FontWeight.w600),
+                                      textAlign: TextAlign.left,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+
+                                    Container(
+                                      height: 30,
+                                      child: IconButton(
+                                        icon: Icon(FontAwesomeIcons.share,
+                                            size: 17, color: Color(0xFF27AE60)),
+                                        onPressed: () {
+                                          InfoGraphicsServices().shareInfoGraphics(
+                                              document['title'], document['images']);
+                                        },
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Text(
+                                  document['title'],
+                                  style: TextStyle(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w600),
+                                  textAlign: TextAlign.left,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
                           ),
-                          SizedBox(height: 8),
-                          Text(
-                            unixTimeStampToDateWithoutDay(
-                                document['published_date'].seconds),
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 12.0,
-                                fontWeight: FontWeight.w600),
-                            textAlign: TextAlign.left,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          )
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ),
-                Container(
-                  child: IconButton(
-                    icon: Icon(FontAwesomeIcons.share,
-                        size: 17, color: Color(0xFF27AE60)),
-                    onPressed: () {
-                      InfoGraphicsServices().shareInfoGraphics(
-                          document['title'], document['images']);
-                    },
-                  ),
-                )
-              ],
-            ),
-          );
-        });
+                  SizedBox(height: 20)
+                ],
+              ),
+            );
+          }),
+    );
   }
 }
