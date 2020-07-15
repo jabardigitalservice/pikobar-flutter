3f1e058803916c503bd00379bacd13685879edd4
Angga <gaganugraha33@gmail.com>
2020-07-15 17:29:22 +0700

    fix & implement component
    - fix component
    - implement to menu CheckDistibutionDetailScreen
    - implement to menu Survey

--- lib/components/Announcement.dart
+++ lib/components/Announcement.dart
@@ -7,9 +7,6 @@ import 'package:pikobar_flutter/constants/Dictionary.dart';
 import 'package:pikobar_flutter/constants/FontsFamily.dart';
 import 'package:pikobar_flutter/environment/Environment.dart';
 import 'package:html/dom.dart' as dom;
-import 'package:pikobar_flutter/repositories/AuthRepository.dart';
-import 'package:pikobar_flutter/screens/login/LoginScreen.dart';
-import 'package:pikobar_flutter/utilities/BasicUtils.dart';
 import 'package:pikobar_flutter/utilities/OpenChromeSapariBrowser.dart';
 
 class Announcement extends StatelessWidget {
@@ -18,9 +15,19 @@ class Announcement extends StatelessWidget {
   final BuildContext context;
   final OnLinkTap onLinkTap;
   final String actionUrl;
+  final TextStyle textStyleTitle;
+  final TextStyle textStyleContent;
+  final TextStyle textStyleMoreDetail;
 
   Announcement(
-      {this.title, this.content, this.context, this.onLinkTap, this.actionUrl});
+      {this.title,
+      this.content,
+      this.context,
+      this.onLinkTap,
+      this.actionUrl,
+      this.textStyleTitle,
+      this.textStyleContent,
+      this.textStyleMoreDetail});
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +35,8 @@ class Announcement extends StatelessWidget {
       width: (MediaQuery.of(context).size.width),
       margin: EdgeInsets.only(left: 5, right: 5),
       decoration: BoxDecoration(
-          color: Color(0xffFFF3CC), borderRadius: BorderRadius.circular(8.0)),
+          color: ColorBase.announcementBackgroundColor,
+          borderRadius: BorderRadius.circular(8.0)),
       child: Stack(
         children: <Widget>[
           Image.asset('${Environment.imageAssets}intersect.png', width: 73),
@@ -37,33 +45,44 @@ class Announcement extends StatelessWidget {
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(
-                    title,
-                    style: TextStyle(
-                        fontSize: 13.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: FontsFamily.lato),
-                  ),
-                  SizedBox(height: 10),
+                  title != null && title.isNotEmpty
+                      ? Text(
+                          title,
+                          style: textStyleTitle != null
+                              ? textStyleTitle
+                              : TextStyle(
+                                  fontSize: 13.0,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: FontsFamily.lato),
+                        )
+                      : Container(),
+                  title != null && title.isNotEmpty
+                      ? SizedBox(height: 10)
+                      : Container(),
                   actionUrl != null && actionUrl.isNotEmpty
                       ? RichText(
                           text: TextSpan(children: [
                             TextSpan(
                               text: content,
-                              style: TextStyle(
-                                  fontSize: 12.0,
-                                  color: Colors.grey[600],
-                                  fontFamily: FontsFamily.lato),
+                              style: textStyleContent != null
+                                  ? textStyleContent
+                                  : TextStyle(
+                                      fontSize: 12.0,
+                                      height: 1.4,
+                                      color: Colors.grey[600],
+                                      fontFamily: FontsFamily.lato),
                             ),
                             TextSpan(
                                 text: Dictionary.moreDetail,
-                                style: TextStyle(
-                                    fontSize: 12.0,
-                                    height: 1.5,
-                                    color: ColorBase.green,
-                                    fontFamily: FontsFamily.lato,
-                                    fontWeight: FontWeight.bold),
+                                style: textStyleMoreDetail != null
+                                    ? textStyleMoreDetail
+                                    : TextStyle(
+                                        fontSize: 12.0,
+                                        height: 1.4,
+                                        color: ColorBase.green,
+                                        fontFamily: FontsFamily.lato,
+                                        fontWeight: FontWeight.bold),
                                 recognizer: TapGestureRecognizer()
                                   ..onTap = () {
                                     openChromeSafariBrowser(url: actionUrl);
@@ -72,11 +91,13 @@ class Announcement extends StatelessWidget {
                         )
                       : Html(
                           data: content,
-                          defaultTextStyle: TextStyle(
-                              color: Color(0xff828282),
-                              height: 1.5,
-                              fontSize: 12.0,
-                              fontFamily: FontsFamily.lato),
+                          defaultTextStyle: textStyleContent != null
+                              ? textStyleContent
+                              : TextStyle(
+                                  color: Colors.grey[600],
+                                  height: 1.4,
+                                  fontSize: 12.0,
+                                  fontFamily: FontsFamily.lato),
                           customTextAlign: (dom.Node node) {
                             return TextAlign.justify;
                           },
