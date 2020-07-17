7074a6f40aedf18d29cc8c0d0ec9f5d3c1db4a8a
aldi <aldirohman@live.com>
2020-07-17 13:05:44 +0700

    add global constants url third party

--- lib/constants/UrlThirdParty.dart
+++ lib/constants/UrlThirdParty.dart
@@ -1,19 +1,17 @@
-class UrlThirdParty {
-  static const String pathPlaystore =
-      'https://play.google.com/store/apps/details?id=id.go.jabarprov.pikobar';
-  static const String urlDonation = 'https://kitabisa.com/campaign/jabarlawancorona';
-  static const String urlLogisticsInfo = 'https://logistik.pikobar.jabarprov.go.id/#/landing-page';
-  static const String urlCoronaInfo = 'https://pikobar.jabarprov.go.id/';
-  static const String urlCoronaInfoData = 'https://pikobar.jabarprov.go.id/data';
-  static const String urlCoronaEscort = 'https://kawalcovid19.id/';
-  static const String urlWorldCoronaInfo = 'https://coronavirus.thebaselab.com/';
-  static const String urlIGSaberHoax = 'https://www.instagram.com/jabarsaberhoaks/';
-  static const String urlCaseReport = 'https://lapor.pikobar.jabarprov.go.id';
-  static const String urlQNA = 'https://forum.pikobar.jabarprov.go.id';
-  static const String urlBansos = 'https://bansos.pikobar.jabarprov.go.id';
-  static const String urlSelfTracing = '';
-  static const String urlVolunteer = 'https://indorelawan.org/p/pikobar';
-  static const String urlSelfDiagnose = 'http://dev.engine.prixa.ai/';
-  static const String urlGoogle = 'https://www.google.com';
-  static const String urlTracking = 'https://tracking.pikobar.jabarprov.go.id';
-}
+
+  const String kUrlDonation = 'https://kitabisa.com/campaign/jabarlawancorona';
+  const String kUrlLogisticsInfo = 'https://logistik.pikobar.jabarprov.go.id/#/landing-page';
+  const String kUrlCoronaInfo = 'https://pikobar.jabarprov.go.id/';
+  const String kUrlCoronaInfoData = 'https://pikobar.jabarprov.go.id/data';
+  const String kUrlCoronaEscort = 'https://kawalcovid19.id/';
+  const String kUrlWorldCoronaInfo = 'https://coronavirus.thebaselab.com/';
+  const String kUrlIGSaberHoax = 'https://www.instagram.com/jabarsaberhoaks/';
+  const String kUrlCaseReport = 'https://lapor.pikobar.jabarprov.go.id';
+  const String kUrlQNA = 'https://forum.pikobar.jabarprov.go.id';
+  const String kUrlBansos = 'https://bansos.pikobar.jabarprov.go.id';
+  const String kUrlSelfTracing = '';
+  const String kUrlVolunteer = 'https://indorelawan.org/p/pikobar';
+  const String kUrlSelfDiagnose = 'http://dev.engine.prixa.ai/';
+  const String kUrlGoogle = 'https://www.google.com';
+  const String kUrlTracking = 'https://tracking.pikobar.jabarprov.go.id';
+  
