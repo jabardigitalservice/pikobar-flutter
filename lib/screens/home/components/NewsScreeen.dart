6f0e3acfbe4ad8d854cf6a59cdcb5fa5ca336feb
Angga <gaganugraha33@gmail.com>
2020-11-16 15:03:02 +0700

    change size image

--- lib/screens/home/components/NewsScreeen.dart
+++ lib/screens/home/components/NewsScreeen.dart
@@ -460,14 +460,14 @@ class _NewsScreenState extends State<NewsScreen> {
             elevation: 0,
             color: Colors.white,
             child: Container(
-              padding: EdgeInsets.only(left: 5, right: 5, top: 17, bottom: 17),
+              padding: EdgeInsets.only(left: 5, right: 5, top: 12, bottom: 12),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   Container(
-                    width: 70,
-                    height: 80,
+                    width: MediaQuery.of(context).size.width-45,
+                    height: 300,
                     child: widget.news == Dictionary.importantInfo ||
                             data.published
                         ? Stack(
@@ -536,64 +536,64 @@ class _NewsScreenState extends State<NewsScreen> {
                             ),
                           ),
                   ),
-                  Container(
-                    padding: const EdgeInsets.only(left: 10.0),
-                    width: MediaQuery.of(context).size.width - 120,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          data.title,
-                          style: TextStyle(
-                              fontSize: 16.0, fontWeight: FontWeight.w600),
-                          textAlign: TextAlign.left,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        Container(
-                            padding: EdgeInsets.only(top: 5.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Expanded(
-                                  child: Container(
-                                    child: Row(
-                                      children: <Widget>[
-                                        Image.network(
-                                          data.newsChannelIcon,
-                                          width: 25.0,
-                                          height: 25.0,
-                                        ),
-                                        SizedBox(width: 3.0),
-                                        Expanded(
-                                          child: Text(
-                                            data.newsChannel,
-                                            overflow: TextOverflow.ellipsis,
-                                            maxLines: 2,
-                                            style: TextStyle(
-                                                fontSize: 10.0,
-                                                color: Colors.grey),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                widget.news != Dictionary.importantInfo &&
-                                        data.newsChannel.isNotEmpty
-                                    ? Text(
-                                        unixTimeStampToDateTime(
-                                            data.publishedAt),
-                                        style: TextStyle(
-                                            fontSize: 10.0, color: Colors.grey),
-                                      )
-                                    : Container(),
-                              ],
-                            )),
-                      ],
-                    ),
-                  ),
+                  // Container(
+                  //   padding: const EdgeInsets.only(left: 10.0),
+                  //   width: MediaQuery.of(context).size.width - 120,
+                  //   child: Column(
+                  //     mainAxisAlignment: MainAxisAlignment.start,
+                  //     crossAxisAlignment: CrossAxisAlignment.start,
+                  //     children: <Widget>[
+                  //       Text(
+                  //         data.title,
+                  //         style: TextStyle(
+                  //             fontSize: 16.0, fontWeight: FontWeight.w600),
+                  //         textAlign: TextAlign.left,
+                  //         maxLines: 2,
+                  //         overflow: TextOverflow.ellipsis,
+                  //       ),
+                  //       Container(
+                  //           padding: EdgeInsets.only(top: 5.0),
+                  //           child: Row(
+                  //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //             children: <Widget>[
+                  //               Expanded(
+                  //                 child: Container(
+                  //                   child: Row(
+                  //                     children: <Widget>[
+                  //                       Image.network(
+                  //                         data.newsChannelIcon,
+                  //                         width: 25.0,
+                  //                         height: 25.0,
+                  //                       ),
+                  //                       SizedBox(width: 3.0),
+                  //                       Expanded(
+                  //                         child: Text(
+                  //                           data.newsChannel,
+                  //                           overflow: TextOverflow.ellipsis,
+                  //                           maxLines: 2,
+                  //                           style: TextStyle(
+                  //                               fontSize: 10.0,
+                  //                               color: Colors.grey),
+                  //                         ),
+                  //                       )
+                  //                     ],
+                  //                   ),
+                  //                 ),
+                  //               ),
+                  //               widget.news != Dictionary.importantInfo &&
+                  //                       data.newsChannel.isNotEmpty
+                  //                   ? Text(
+                  //                       unixTimeStampToDateTime(
+                  //                           data.publishedAt),
+                  //                       style: TextStyle(
+                  //                           fontSize: 10.0, color: Colors.grey),
+                  //                     )
+                  //                   : Container(),
+                  //             ],
+                  //           )),
+                  //     ],
+                  //   ),
+                  // ),
                 ],
               ),
             ),
@@ -609,10 +609,6 @@ class _NewsScreenState extends State<NewsScreen> {
                   <String, dynamic>{'title': data.title});
             },
           ),
-        ),
-        Container(
-          height: 10,
-          color: ColorBase.grey,
         ),
       ],
     ));
