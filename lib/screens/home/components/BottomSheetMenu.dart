da7c49b337297b90c7439296adaa97fb268a2ab9
Risa Herdianto <aputra.zha@gmail.com>
2020-11-18 17:38:24 +0700

    Final bottom sheet menu

--- lib/screens/home/components/BottomSheetMenu.dart
+++ lib/screens/home/components/BottomSheetMenu.dart
@@ -1,29 +1,17 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:pikobar_flutter/components/CustomBottomSheet.dart';
-import 'package:pikobar_flutter/constants/Dimens.dart';
-import 'package:pikobar_flutter/constants/FontsFamily.dart';
-import 'package:pikobar_flutter/environment/Environment.dart';
-import 'package:pikobar_flutter/screens/home/components/MenuListOther.dart';
-import 'package:pikobar_flutter/utilities/SliverGrideDelegate.dart';
+import 'package:pikobar_flutter/screens/home/components/MenuList.dart';
 
 class BottomSheetMenu {
   const BottomSheetMenu._();
 
   static Future<void> showBottomSheetMenu(
-      {@required BuildContext context}) async {
-
-
-
-    return showWidgetBottomSheet(
+          {@required BuildContext context}) async =>
+      showWidgetBottomSheet(
         context: context,
         isScrollControlled: true,
-        child: MenuList());
-  }
-
-
-
-
+        child: MenuList(),
+      );
 }
