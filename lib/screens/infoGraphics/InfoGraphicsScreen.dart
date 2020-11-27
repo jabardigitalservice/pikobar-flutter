fc1a7591bb714f44c76f8c4bcac791e72ef22611
Angga <gaganugraha33@gmail.com>
2020-11-27 15:22:25 +0700

    redesign

--- lib/screens/infoGraphics/InfoGraphicsScreen.dart
+++ lib/screens/infoGraphics/InfoGraphicsScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
@@ -7,12 +9,11 @@ import 'package:pikobar_flutter/blocs/infographics/Bloc.dart';
 import 'package:pikobar_flutter/components/CustomAppBar.dart';
 import 'package:pikobar_flutter/components/CustomBubbleTab.dart';
 import 'package:pikobar_flutter/components/EmptyData.dart';
-import 'package:pikobar_flutter/components/PikobarPlaceholder.dart';
 import 'package:pikobar_flutter/components/Skeleton.dart';
 import 'package:pikobar_flutter/constants/Analytics.dart';
 import 'package:pikobar_flutter/constants/Colors.dart';
 import 'package:pikobar_flutter/constants/Dictionary.dart';
-import 'package:pikobar_flutter/constants/FontsFamily.dart';
+import 'package:pikobar_flutter/constants/Dimens.dart';
 import 'package:pikobar_flutter/constants/collections.dart';
 import 'package:pikobar_flutter/environment/Environment.dart';
 import 'package:pikobar_flutter/screens/infoGraphics/DetailInfoGraphicScreen.dart';
@@ -26,6 +27,10 @@ class InfoGraphicsScreen extends StatefulWidget {
 
 class _InfoGraphicsScreenState extends State<InfoGraphicsScreen> {
   InfoGraphicsListBloc _infoGraphicsListBloc = InfoGraphicsListBloc();
+  TextEditingController _searchController = TextEditingController();
+  ScrollController _scrollController;
+  Timer _debounce;
+  String searchQuery;
 
   List<String> listItemTitleTab = [
     Dictionary.titleLatestNews,
@@ -48,41 +53,55 @@ class _InfoGraphicsScreenState extends State<InfoGraphicsScreen> {
   @override
   void initState() {
     AnalyticsHelper.setCurrentScreen(Analytics.infoGraphics);
+    _scrollController = ScrollController()..addListener(() => setState(() {}));
+    _searchController.addListener((() {
+      _onSearchChanged();
+    }));
     super.initState();
+  }
+
+  bool get _showTitle {
+    return _scrollController.hasClients &&
+        _scrollController.offset >
+            0.16 * MediaQuery.of(context).size.height - (kToolbarHeight * 1.8);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomAppBar.defaultAppBar(title: Dictionary.infoGraphics),
         body: MultiBlocProvider(
-          providers: [
-            BlocProvider<InfoGraphicsListBloc>(
-              create: (context) => _infoGraphicsListBloc
-                ..add(InfoGraphicsListLoad(
-                    infoGraphicsCollection: kInfographics)),
-            ),
-          ],
-          child: Container(
-              child: CustomBubbleTab(
-            listItemTitleTab: listItemTitleTab,
-            indicatorColor: ColorBase.green,
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.grey,
-            onTap: (index) {
-              setState(() {});
-              _infoGraphicsListBloc.add(InfoGraphicsListLoad(
-                  infoGraphicsCollection: listCollectionData[index]));
-              AnalyticsHelper.setLogEvent(analyticsData[index]);
-            },
-            tabBarView: <Widget>[
-              _buildInfoGraphic(),
-              _buildInfoGraphic(),
-              _buildInfoGraphic(),
-            ],
-            heightTabBarView: MediaQuery.of(context).size.height - 148,
-          )),
-        ));
+      providers: [
+        BlocProvider<InfoGraphicsListBloc>(
+          create: (context) => _infoGraphicsListBloc
+            ..add(InfoGraphicsListLoad(infoGraphicsCollection: kInfographics)),
+        ),
+      ],
+      child: Container(
+          child: CustomBubbleTab(
+        isStickyHeader: true,
+        titleHeader: Dictionary.infoGraphics,
+        listItemTitleTab: listItemTitleTab,
+        indicatorColor: ColorBase.green,
+        labelColor: Colors.white,
+        showTitle: _showTitle,
+        searchBar: CustomAppBar.buildSearchField(
+            _searchController, Dictionary.searchInformation, updateSearchQuery),
+        unselectedLabelColor: Colors.grey,
+        scrollController: _scrollController,
+        onTap: (index) {
+          setState(() {});
+          _infoGraphicsListBloc.add(InfoGraphicsListLoad(
+              infoGraphicsCollection: listCollectionData[index]));
+          AnalyticsHelper.setLogEvent(analyticsData[index]);
+        },
+        tabBarView: <Widget>[
+          _buildInfoGraphic(),
+          _buildInfoGraphic(),
+          _buildInfoGraphic(),
+        ],
+        heightTabBarView: MediaQuery.of(context).size.height - 148,
+      )),
+    ));
   }
 
   Widget _buildInfoGraphic() {
@@ -96,133 +115,150 @@ class _InfoGraphicsScreenState extends State<InfoGraphicsScreen> {
   }
 
   Widget _buildContent(List<DocumentSnapshot> listData) {
+    if (searchQuery != null) {
+      listData = listData
+          .where((test) =>
+              test['title'].toLowerCase().contains(searchQuery.toLowerCase()))
+          .toList();
+    }
+
     return listData.isNotEmpty
-        ? GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, childAspectRatio: 0.7),
+        ? ListView.builder(
             shrinkWrap: true,
             itemCount: listData.length,
-            padding: EdgeInsets.only(bottom: 20.0, left: 14.0),
+            padding: EdgeInsets.only(bottom: 20.0),
             itemBuilder: (_, int index) {
               return _cardContent(listData[index]);
             },
           )
-        : EmptyData(
-            message: Dictionary.emptyData,
-            desc: '',
-            isFlare: false,
-            image: "${Environment.imageAssets}not_found.png",
+        : ListView(
+            children: [
+              EmptyData(
+                message: Dictionary.emptyData,
+                desc: Dictionary.descEmptyData,
+                isFlare: false,
+                image: "${Environment.imageAssets}not_found.png",
+              )
+            ],
           );
   }
 
   _buildLoading() {
-    return Container(
-        child: GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, childAspectRatio: 0.7),
-      shrinkWrap: true,
-      itemCount: 10,
-      padding: EdgeInsets.only(bottom: 20.0, left: 5.0, right: 5.0),
-      itemBuilder: (_, int index) {
-        return Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          elevation: 1.5,
-          margin: EdgeInsets.only(top: 10, bottom: 10, left: 5, right: 5),
-          clipBehavior: Clip.antiAlias,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.20,
-                decoration: BoxDecoration(shape: BoxShape.circle),
-                child: Skeleton(
-                  width: MediaQuery.of(context).size.width,
-                  padding: 10.0,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 10.0, right: 14.0, top: 14.0, bottom: 14.0),
+    return SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Container(
+          margin: EdgeInsets.only(bottom: 10.0),
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: 6,
+            padding: const EdgeInsets.all(10.0),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                padding: EdgeInsets.only(bottom: 20, left: 10, right: 10),
+                height: 300.0,
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Skeleton(
-                            height: 20.0,
-                            width: MediaQuery.of(context).size.width / 1.4,
-                            padding: 10.0,
-                          ),
-                          SizedBox(height: 8),
-                          Skeleton(
-                            height: 20.0,
-                            width: MediaQuery.of(context).size.width / 2,
-                            padding: 10.0,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Container(
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
                       child: Skeleton(
-                        height: 20.0,
-                        width: 20,
-                      ),
-                    )
+                          width: MediaQuery.of(context).size.width - 40),
+                    ),
                   ],
                 ),
-              ),
-            ],
+              );
+            },
           ),
-        );
-      },
-    ));
+        ),
+      ),
+    );
   }
 
   Widget _cardContent(DocumentSnapshot data) {
     return Container(
-      margin: EdgeInsets.only(right: 14, top: 10, bottom: 0),
-      child: Column(
-        children: <Widget>[
-          InkWell(
+        child: Column(
+      children: <Widget>[
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: RaisedButton(
+            elevation: 0,
+            color: Colors.white,
             child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.20,
-              decoration: BoxDecoration(shape: BoxShape.circle),
-              child: CachedNetworkImage(
-                imageUrl: data['images'][0] ?? '',
-                imageBuilder: (context, imageProvider) => Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                    image: DecorationImage(
-                      alignment: Alignment.topCenter,
-                      image: imageProvider,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                placeholder: (context, url) => Center(
-                    heightFactor: 10.2, child: CupertinoActivityIndicator()),
-                errorWidget: (context, url, error) => Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(5.0),
-                        topRight: Radius.circular(5.0)),
-                  ),
-                  child: PikobarPlaceholder(),
-                ),
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Stack(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width - 35,
+                        height: 300,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: CachedNetworkImage(
+                            imageUrl: data['images'][0] ?? '',
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) => Center(
+                                heightFactor: 4.2,
+                                child: CupertinoActivityIndicator()),
+                            errorWidget: (context, url, error) => Container(
+                                height:
+                                    MediaQuery.of(context).size.height / 3.3,
+                                color: Colors.grey[200],
+                                child: Image.asset(
+                                    '${Environment.iconAssets}pikobar.png',
+                                    fit: BoxFit.fitWidth)),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width - 35,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          color: Colors.black12.withOpacity(0.2),
+                          shape: BoxShape.rectangle,
+                          borderRadius:
+                              BorderRadius.circular(Dimens.dialogRadius),
+                        ),
+                      ),
+                      Positioned(
+                        left: 10,
+                        right: 10,
+                        bottom: 0,
+                        top: 215,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              unixTimeStampToDateTime(
+                                  data['published_date'].seconds),
+                              style: TextStyle(
+                                  fontSize: 16.0, color: Colors.white),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              data['title'],
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white),
+                              textAlign: TextAlign.left,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
             ),
-            onTap: () {
+            onPressed: () {
               Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) =>
                       DetailInfoGraphicScreen(dataInfoGraphic: data)));
@@ -231,67 +267,40 @@ class _InfoGraphicsScreenState extends State<InfoGraphicsScreen> {
                   <String, dynamic>{'title': data['title']});
             },
           ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 8.0, right: 9.0, top: 14.0, bottom: 5.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) =>
-                              DetailInfoGraphicScreen(dataInfoGraphic: data)));
+        ),
+      ],
+    ));
+  }
 
-                      AnalyticsHelper.setLogEvent(
-                          Analytics.tappedInfoGraphicsDetail,
-                          <String, dynamic>{'title': data['title']});
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Expanded(
-                                child: Text(
-                                  unixTimeStampToDateTime(
-                                      data['published_date'].seconds),
-                                  style: TextStyle(
-                                      color: Colors.grey,
-                                      fontFamily: FontsFamily.lato,
-                                      fontSize: 10.0,
-                                      fontWeight: FontWeight.w600),
-                                  textAlign: TextAlign.left,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              )
-                            ]),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          data['title'],
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: FontsFamily.lato,
-                          ),
-                          textAlign: TextAlign.left,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+  void _onSearchChanged() {
+    if (_debounce?.isActive ?? false) _debounce.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      if (_searchController.text.trim().isNotEmpty) {
+        setState(() {
+          searchQuery = _searchController.text;
+        });
+      } else {
+        _clearSearchQuery();
+      }
+    });
+  }
+
+  void updateSearchQuery(String newQuery) {
+    setState(() {
+      searchQuery = newQuery;
+    });
+  }
+
+  void _clearSearchQuery() {
+    setState(() {
+      _searchController.clear();
+      updateSearchQuery(null);
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 }
