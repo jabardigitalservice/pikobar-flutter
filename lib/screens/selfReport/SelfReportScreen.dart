25dba0aca3ca2954cfcba14c75ae8989612dfa42
jaluowen <48124139+jaluowen@users.noreply.github.com>
2021-02-01 11:30:06 +0700

    Fix Stuck On Scrolling

--- lib/screens/selfReport/SelfReportScreen.dart
+++ lib/screens/selfReport/SelfReportScreen.dart
@@ -15,11 +15,13 @@ import 'package:pikobar_flutter/components/CustomAppBar.dart';
 import 'package:pikobar_flutter/components/CustomBottomSheet.dart';
 import 'package:pikobar_flutter/components/DialogRequestPermission.dart';
 import 'package:pikobar_flutter/components/DialogTextOnly.dart';
+import 'package:pikobar_flutter/components/ErrorContent.dart';
 import 'package:pikobar_flutter/constants/Analytics.dart';
 import 'package:pikobar_flutter/constants/Colors.dart';
 import 'package:pikobar_flutter/constants/Dictionary.dart';
 import 'package:pikobar_flutter/constants/FontsFamily.dart';
 import 'package:pikobar_flutter/constants/Navigation.dart';
+import 'package:pikobar_flutter/constants/collections.dart';
 import 'package:pikobar_flutter/environment/Environment.dart';
 import 'package:pikobar_flutter/repositories/AuthRepository.dart';
 import 'package:pikobar_flutter/repositories/GeocoderRepository.dart';
@@ -135,7 +137,7 @@ class _SelfReportScreenState extends State<SelfReportScreen> {
           child: Scaffold(
             key: _scaffoldKey,
             appBar: CustomAppBar.animatedAppBar(
-              showTitle: _showTitle,
+              showTitle: true,
               title: Dictionary.titleSelfReport,
             ),
             backgroundColor: Colors.white,
@@ -152,29 +154,31 @@ class _SelfReportScreenState extends State<SelfReportScreen> {
                 );
               } else if (state is AuthenticationAuthenticated ||
                   state is AuthenticationLoading) {
-                return BlocBuilder<ProfileBloc, ProfileState>(builder: (
-                  BuildContext context,
-                  ProfileState state,
-                ) {
-                  if (state is ProfileLoading) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (state is ProfileLoaded) {
-                    ProfileLoaded _getProfile = state;
-                    return _getProfile.profile.exists
-                        ? _buildContent(_getProfile.profile)
-                        : _buildContent(null);
-                  } else {
-                    _profileBloc.add(ProfileLoad(
-                        uid: profileLoaded != null
+                return StreamBuilder<DocumentSnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection(kUsers)
+                        .doc(profileLoaded != null
                             ? profileLoaded.record.uid
-                            : null));
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                });
+                            : null)
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<DocumentSnapshot> snapshot) {
+                      if (snapshot.hasError)
+                        // Show error ui when unable to get data
+                        return ErrorContent(error: snapshot.error);
+                      switch (snapshot.connectionState) {
+                        // Show loading while get data
+                        case ConnectionState.waiting:
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        default:
+                          // Show content when data is ready
+                          return snapshot.data.exists
+                              ? _buildContent(snapshot.data)
+                              : _buildContent(null);
+                      }
+                    });
               } else if (state is AuthenticationFailure ||
                   state is AuthenticationLoading) {
                 return OnBoardingLoginScreen(
@@ -220,22 +224,7 @@ class _SelfReportScreenState extends State<SelfReportScreen> {
     return Padding(
         padding: EdgeInsets.all(10),
         child: ListView(
-          controller: _scrollController,
           children: <Widget>[
-            AnimatedOpacity(
-              opacity: _showTitle ? 0.0 : 1.0,
-              duration: Duration(milliseconds: 250),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Text(
-                  Dictionary.titleSelfReport,
-                  style: TextStyle(
-                      fontFamily: FontsFamily.lato,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
             hasLogin
                 ? !HealthCheck()
                             .isUserHealty(getField(state, 'health_status')) &&
