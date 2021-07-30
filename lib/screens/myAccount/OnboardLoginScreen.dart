21fd554d284c43fe9f8d0aa34c85b4d7f9f12f5d
Randi Julias <jaluowen@gmail.com>
2021-07-30 12:36:08 +0700

    Add Const

--- lib/screens/myAccount/OnboardLoginScreen.dart
+++ lib/screens/myAccount/OnboardLoginScreen.dart
@@ -67,7 +67,7 @@ class _OnBoardingLoginScreenState extends State<OnBoardingLoginScreen> {
         children: <Widget>[
           widget.showTitle
               ? Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: Text(
                     Dictionary.profile,
                     style: TextStyle(
@@ -88,7 +88,7 @@ class _OnBoardingLoginScreenState extends State<OnBoardingLoginScreen> {
                 children: <Widget>[
                   Image.asset('${Environment.logoAssets}pikobar_logo_flat.png',
                       width: 80, height: 80),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
                   Container(
@@ -105,8 +105,8 @@ class _OnBoardingLoginScreenState extends State<OnBoardingLoginScreen> {
                 ],
               ),
               Container(
-                margin:
-                    EdgeInsets.fromLTRB(Dimens.padding, 20, Dimens.padding, 0),
+                margin: const EdgeInsets.fromLTRB(
+                    Dimens.padding, 20, Dimens.padding, 0),
                 child: Text(
                   Dictionary.titleOnBoardingLogin,
                   textAlign: TextAlign.center,
@@ -130,11 +130,11 @@ class _OnBoardingLoginScreenState extends State<OnBoardingLoginScreen> {
               Container(
                 width: size.width,
                 height: 40,
-                margin: EdgeInsets.fromLTRB(Dimens.padding, Dimens.padding,
-                    Dimens.padding, Dimens.padding),
+                margin: const EdgeInsets.fromLTRB(Dimens.padding,
+                    Dimens.padding, Dimens.padding, Dimens.padding),
                 child: RaisedButton(
                     splashColor: Colors.lightGreenAccent,
-                    padding: EdgeInsets.all(0),
+                    padding: const EdgeInsets.all(0),
                     color: isAgree ? ColorBase.green : Colors.grey,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(Dimens.borderRadius),
@@ -164,8 +164,8 @@ class _OnBoardingLoginScreenState extends State<OnBoardingLoginScreen> {
                     }),
               ),
               Container(
-                margin:
-                    EdgeInsets.fromLTRB(Dimens.padding, 0, Dimens.padding, 50),
+                margin: const EdgeInsets.fromLTRB(
+                    Dimens.padding, 0, Dimens.padding, 50),
                 child: Text(
                   '${Dictionary.versionText} ' + _versionText,
                   textAlign: TextAlign.center,
@@ -192,7 +192,7 @@ class _OnBoardingLoginScreenState extends State<OnBoardingLoginScreen> {
       dataPrivacy =
           json.decode(remoteConfig.getString(FirebaseConfig.dataPrivacy));
       return Container(
-        margin: EdgeInsets.fromLTRB(10, 0, Dimens.padding, 10),
+        margin: const EdgeInsets.fromLTRB(10, 0, Dimens.padding, 10),
         child: Row(
           children: <Widget>[
             Checkbox(
@@ -265,19 +265,19 @@ class _OnBoardingLoginScreenState extends State<OnBoardingLoginScreen> {
         elevation: 60,
         builder: (BuildContext context) {
           return Container(
-            margin: EdgeInsets.only(bottom: 20),
+            margin: const EdgeInsets.only(bottom: 20),
             child: Wrap(
               alignment: WrapAlignment.center,
               children: <Widget>[
                 Container(
-                  margin: EdgeInsets.only(top: 14),
+                  margin: const EdgeInsets.only(top: 14),
                   color: Colors.black,
                   height: 1.5,
                   width: 40,
                 ),
                 Container(
                   alignment: Alignment.topCenter,
-                  padding: EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(20),
                   decoration: BoxDecoration(boxShadow: [
                     BoxShadow(
                       color: Colors.white.withOpacity(05),
@@ -287,11 +287,11 @@ class _OnBoardingLoginScreenState extends State<OnBoardingLoginScreen> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
-                      SizedBox(height: 5),
+                      const SizedBox(height: 5),
                       isAvailable
                           ? Column(children: <Widget>[
                               _signInButton(isApple: true),
-                              SizedBox(
+                              const SizedBox(
                                 height: 16,
                               )
                             ])
