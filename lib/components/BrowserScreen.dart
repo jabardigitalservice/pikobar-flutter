678707aa89e2482b6411fbf46522c2481f275a8a
Randi Julias <jaluowen@gmail.com>
2022-04-20 16:11:36 +0700

    Added Webview Custom Package

--- lib/components/BrowserScreen.dart
+++ lib/components/BrowserScreen.dart
@@ -101,6 +101,7 @@ class _BrowserScreenState extends State<BrowserScreen> {
                             : "Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Mobile/15E148 Safari/604.1",
                       ),
                       android: AndroidInAppWebViewOptions(
+                          useHybridComposition: true,
                           allowContentAccess: true,
                           thirdPartyCookiesEnabled: true,
                           allowFileAccess: true,
